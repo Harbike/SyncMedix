c87d15137e81b321ce23a573767c58777ac42f34
harbike <adenlekehny@gmail.com>
2025-06-23 15:33:26 +0100

    chore: modify page layout and apply clip-path to image section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,87 @@
+import {
+  facebook_icon,
+  instagram_icon,
+  twitter_icon,
+  youtube_icon,
+  linkedln_icon,
+} from "@/assets";
+import { Link } from "react-router-dom";
 
-function Footer()  {
-    return (
-        <>
-        <div className="w-full">
-            <h3>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero consequatur aut repudiandae.</h3>
+function Footer() {
+  const currentYear = new Date().getFullYear();
+
+  const handleClick = (e: React.MouseEvent<HTMLImageElement>) => {
+    const { id } = e.target as HTMLImageElement;
+
+    let url = "https://www.youtube.com/SyncMedic/";
+    if (id === "instagram") {
+      url = "https://www.instagram.com/SyncMedic";
+    } else if (id === "linkedin") {
+      url = "https://www.linkedin.com/company/SyncMedic";
+    } else if (id === "facebook") {
+      url = "https://www.facebook.com/share/SyncMedic";
+    } else url = "https://x.com/SyncMedic"; //twitter
+
+    window.open(url, "_blank");
+  };
+
+  return (
+    <div className="w-full border-t-2 py-2">
+      <div className="flex justify-between items-center md:justify-evenly py-5 px-3">
+        {/* footer navigations */}
+        <div>
+          <ul className="flex flex-col gap-3">
+            <Link to="{/Pulicy>}">
+              <li>Privacy Policy</li>
+            </Link>
+            <Link to="{/Terms>}">
+              <li>Terms of Service</li>
+            </Link>
+            <Link to="{/Settings>}">
+              <li>Cookies Settings</li>
+            </Link>
+          </ul>
         </div>
-        </>
-    )
+        {/* social icons */}
+        <div className="flex gap-2">
+          <img
+            src={facebook_icon}
+            className="cursor-pointer w-6 h-6"
+            id="facebook"
+            onClick={handleClick}
+          />
+          <img
+            src={instagram_icon}
+            className="cursor-pointer w-6 h-6"
+            id="instagram"
+            onClick={handleClick}
+          />
+          <img
+            src={twitter_icon}
+            className="cursor-pointer w-6 h-6"
+            id="twitter"
+            onClick={handleClick}
+          />
+          <img
+            src={linkedln_icon}
+            className="cursor-pointer w-6 h-6"
+            id="linkedln"
+            onClick={handleClick}
+          />
+          <img
+            src={youtube_icon}
+            className="cursor-pointer w-6 h-6"
+            id="youtube"
+            onClick={handleClick}
+          />
+        </div>
+      </div>
+
+      <p className="text-xs pt-5 text-center">
+        © {currentYear} SyncMedix. All rights reserved.
+      </p>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,51 +1,39 @@
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import { useState } from "react"
+import { test_tubes } from "@/assets";
+import { Button } from "@/components/ui/button";
 
-
-function Home()  {
-const [role, setRole] = useState("patient")
-
+function Home() {
   return (
-    <div className="min-h-screen bg-zinc-200 flex flex-col p-6">
-        <p className="bg-darkNavy underline">One Record. One Care</p>
-      <h1 className="text-2xl font-semibold mb-4 text-center">
-        Access Your Medical Records Anytime, Anywhere
-      </h1>
+    <div className="bg-brand-100 flex flex-col mt-6 [clip-path:polygon(0%_0%,_100%_0%,_100%_100%,_0%_97%)]">
+      <div className="w-full">
+        <img
+          src={test_tubes}
+          alt="test-tubes"
+          className="w-full rounded-lg [clip-path:polygon(0%_0%,_100%_0%,_100%_90%,_0%_100%)]"
+        />
+      </div>
 
-      <Tabs defaultValue="patient" onValueChange={setRole}>
-        <TabsList className="mb-6">
-          <TabsTrigger value="patient">Patient</TabsTrigger>
-          <TabsTrigger value="doctor">Doctor</TabsTrigger>
-          <TabsTrigger value="admin">Admin</TabsTrigger>
-        </TabsList>
-      </Tabs>
-
-      <div className="space-y-4">
-        <Input placeholder="Email" type="email" />
-        <Input placeholder="Password" type="password" />
-
-        <Button className="w-full">Sign in as {role}</Button>
-
-        <div className="text-center text-sm text-muted-foreground">
-          <a href="#" className="underline">Need help?</a>
+      <div className="px-2 py-5">
+        <div className="text-brand-primary">
+          <p className="font-semibold py-2">One Record. One Care</p>
+          <h1 className="text-4xl font-bold mb-4">
+            Access Your Medical Records Anytime, Anywhere
+          </h1>
+          <p className="text-sm w-4/5">
+            Log in to manage your health records, connect with your doctor, and
+            access critical information in emergencies.
+          </p>
+        </div>
+        <div className="flex gap-4 px-2 py-4">
+          <Button className="border border-brand-primary bg-brand-primary text-white text-xs rounded-full cursor-pointer hover:bg-brand-300">
+            Log In
+          </Button>
+          <Button className="border border-brand-primary text-brand-primary text-xs rounded-full cursor-pointer hover:bg-blue-100">
+            Emergency Access
+          </Button>
         </div>
       </div>
-
-      <div className="mt-10 w-full max-w-md border-t border-zinc-700 pt-6">
-        <p className="text-sm mb-3 text-zinc-300">🚨 Emergency access</p>
-        <Input placeholder="Enter Emergency PIN" />
-        <Button variant="destructive" className="w-full mt-2">
-          View Critical Info
-        </Button>
-        <p className="text-xs text-muted-foreground mt-2">
-          Access blood group, genotype, allergies, and emergency contacts.
-        </p>
-      </div>
     </div>
-  )
-
+  );
 }
 
-export default Home
+export default Home;
